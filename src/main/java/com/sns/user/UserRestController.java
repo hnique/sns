50c1fc326bb4e267ff1ba4f4c34fd82ac61a286c
hnique <hnique@naver.com>
2023-07-21 02:32:59 +0900

    0718 수업

--- src/main/java/com/sns/user/UserRestController.java
+++ src/main/java/com/sns/user/UserRestController.java
@@ -3,6 +3,8 @@ package com.sns.user;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,12 +24,12 @@ public class UserRestController {
 
 	/**
 	 * 아이디 중복확인 API
+	 * 
 	 * @param loginId
 	 * @return
 	 */
 	@RequestMapping("/is_duplicated_id")
-	public Map<String, Object> isDuplicatedId(
-			@RequestParam("loginId") String loginId) {
+	public Map<String, Object> isDuplicatedId(@RequestParam("loginId") String loginId) {
 
 		Map<String, Object> result = new HashMap<>();
 		result.put("isDuplicatedId", false);
@@ -38,27 +40,71 @@ public class UserRestController {
 
 		if (userEntity != null) {
 			result.put("isDuplicatedId", true);
-		} 
+		}
 
 		return result;
 	}
 
+	/**
+	 * 회원가입 API
+	 * 
+	 * @param loginId
+	 * @param password
+	 * @param name
+	 * @param email
+	 * @return
+	 */
 	@PostMapping("/sign_up")
 	public Map<String, Object> signUp(
 			@RequestParam("loginId") String loginId,
-			@RequestParam("password") String password,
-			@RequestParam("name") String name,
+			@RequestParam("password") String password, @RequestParam("name") String name,
 			@RequestParam("email") String email) {
 
 		// 비밀번호 해싱
 		String hashedPassword = EncryptUtils.md5(password);
 
 		// db insert
+		Integer userId = userBO.addUser(loginId, hashedPassword, name, email);
 
-		// 응답
 		Map<String, Object> result = new HashMap<>();
-		result.put("code", 1);
-		result.put("result", "성공");
+		if (userId != null) {
+			// 응답
+			result.put("code", 1);
+			result.put("result", "성공");
+		} else {
+			result.put("code", 500);
+			result.put("errorMessage", "회원가입에 실패했습니다.");
+		}
+
+		return result;
+	}
+
+	@PostMapping("/sign_in")
+	public Map<String, Object> signIn(
+			@RequestParam("loginId") String loginId,
+			@RequestParam("password") String password, HttpSession session) {
+
+		// password hashing
+		String hashedPassword = EncryptUtils.md5(password);
+
+		// loginId, hashedPassword로 UserEntity => null or 채워져있음
+		UserEntity userEntity = userBO.getUserEntityByLoginIdPassword(loginId, hashedPassword);
+
+		Map<String, Object> result = new HashMap<>();
+		if (userEntity != null) {
+			// 로그인 처리
+			session.setAttribute("userId", userEntity.getId());
+			session.setAttribute("userLoginId", userEntity.getLoginId());
+			session.setAttribute("userName", userEntity.getName());
+
+			result.put("code", 1);
+			result.put("result", "성공");
+		} else {
+			// 로그인 불가
+			result.put("code", 500);
+			result.put("errorMessage", "존재하지 않는 사용자입니다.");
+		}
+
 		return result;
 	}
 }
