9e5f5bbbe2a84f83abb226eef29f906064465790
hnique <hnique@naver.com>
2023-07-21 02:55:27 +0900

    0719 수업

---
+++ src/main/java/com/sns/post/bo/PostBO.java
@@ -0,0 +1,24 @@
+package com.sns.post.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sns.post.dao.PostMapper;
+import com.sns.post.dao.PostRepository;
+import com.sns.post.entity.PostEntity;
+
+@Service
+public class PostBO {
+
+	@Autowired
+	private PostMapper postMapper;  // mybatis 지금 안씀
+
+	@Autowired
+	private PostRepository postRespository; // JPA 
+
+	public List<PostEntity> getPostList() {
+		return postRespository.findAllByOrderByIdDesc();
+	}
+}
