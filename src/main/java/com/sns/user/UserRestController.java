a1fc529a00293954e92de4a8990857a1d510a623
hnique <hnique@naver.com>
2023-07-20 22:00:24 +0900

    0717 수업

--- src/main/java/com/sns/user/UserRestController.java
+++ src/main/java/com/sns/user/UserRestController.java
@@ -4,10 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sns.common.EncryptUtils;
 import com.sns.user.bo.UserBO;
 import com.sns.user.entity.UserEntity;
 
@@ -18,6 +20,11 @@ public class UserRestController {
 	@Autowired
 	private UserBO userBO;
 
+	/**
+	 * 아이디 중복확인 API
+	 * @param loginId
+	 * @return
+	 */
 	@RequestMapping("/is_duplicated_id")
 	public Map<String, Object> isDuplicatedId(
 			@RequestParam("loginId") String loginId) {
@@ -33,6 +40,25 @@ public class UserRestController {
 			result.put("isDuplicatedId", true);
 		} 
 
+		return result;
+	}
+
+	@PostMapping("/sign_up")
+	public Map<String, Object> signUp(
+			@RequestParam("loginId") String loginId,
+			@RequestParam("password") String password,
+			@RequestParam("name") String name,
+			@RequestParam("email") String email) {
+
+		// 비밀번호 해싱
+		String hashedPassword = EncryptUtils.md5(password);
+
+		// db insert
+
+		// 응답
+		Map<String, Object> result = new HashMap<>();
+		result.put("code", 1);
+		result.put("result", "성공");
 		return result;
 	}
 }
